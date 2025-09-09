473c4ce25c8d44e544aa1a1d1ba885f0bb905da6
Nikhil Sagili <nikhilsagili@gmail.com>
2025-09-10 03:12:38 +0530

    fix blank screen

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app/App.tsx'
 import './index.css'
 
-// Single Page Apps for GitHub Pages
-// https://github.com/rafgraph/spa-github-pages
-// Check if we were redirected from a 404 page
-const isRedirectedFromError = /\/\?\//.test(window.location.search);
-if (isRedirectedFromError) {
-  // Remove the redirect query parameter and navigate to the intended page
-  const { pathname, search, hash } = window.location;
-  const route = search.replace(/^\?\//,'').replace(/&/g,'?').replace(/~and~/g,'&');
-  window.history.replaceState(null, '', pathname + route + hash);
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
