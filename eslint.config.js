bba65f03179222c2f801e1c1f2fcb58812f43c47
Nikhil Sagili <nikhilsagili@gmail.com>
2025-09-10 02:35:51 +0530

    Fix ESLint configuration and dependencies for GitHub Actions

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,15 +14,9 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
     },
-  },
-  prettierConfig
+  }
 )
